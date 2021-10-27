1b848636ced3d3559b7ecf85b53a87e713f3923a
Franciskus Simbolon <frans7mbolon@gmail.com>
2021-10-27 22:21:01 +0700

    Menambahkan File

---
+++ tugaslatihan.py
@@ -0,0 +1,16 @@
+print("Mengimput Biodata Diri")
+
+name=input("name:")
+call=input("call:")
+age=input("age:")
+handphone=input("handphone:")
+nim=input("nim:")
+dob=input("dob:")
+address=input("address:")
+
+print("Syalom Horas")
+print("Let me introduce my self. My Name is ",name,"but you can call me",call,"."
+"my nim is",nim,"i was born in",dob,"and i am",age,"years old i am"
+"very glad if you want to invite my address in",address,"so don't"
+"forget to call me before with the handphone",handphone,"")
+print("thank you")
